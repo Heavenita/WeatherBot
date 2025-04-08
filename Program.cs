177b95969beead493abcb81d95364f73cb6304f6
Heavenita <rossihpedro@gmail.com>
2025-04-08 15:52:22 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetEnv;
+using Discord;
+using Discord.WebSocket;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using WheaterBot;
+
+class Program
+{
+    private DiscordSocketClient _client;
+    static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+    public async Task RunBotAsync()
+    {
+        Env.Load("C:\\Users\\Pedro Rossi\\source\\repos\\WheaterBot\\.env"); // Carregar variáveis ​env do arquivo .env
+        string token = Env.GetString("DISCORD_TOKEN"); // Token
+
+        _client = new DiscordSocketClient(new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.Guilds | GatewayIntents.GuildMessages | GatewayIntents.MessageContent
+        });
+        _client.Log += Log;
+        _client.Ready += OnReady;
+        _client.MessageReceived += HandleMessageAsync;
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private Task OnReady()
+    {
+        Console.WriteLine($"{_client.CurrentUser} is connected!");
+        return Task.CompletedTask;
+    }
+
+    private Task Log(LogMessage arg)
+    {
+        Console.WriteLine(arg);
+        return Task.CompletedTask;
+    }
+
+    private async Task HandleMessageAsync(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        if (message.Content.ToLower().StartsWith("!clima")){
+            string[] partes = message.Content.Split(" ", 2);
+            if (partes.Length < 2)
+            {
+                await message.Channel.SendMessageAsync("Digite a cidade. Exemplo: `!clima porto alegre`");
+                return;
+            }
+
+            string cidade = partes[1];
+            string resultado = await WeatherServices.ObterClimaAsync(cidade);
+            await message.Channel.SendMessageAsync(resultado);
+        }
+
+        if (message.Content.ToLower().StartsWith("!ex"))
+        {
+            await message.Channel.SendMessageAsync("Tua ex ta mamando!!");
+            return;
+        }
+    }
+}
---
+++ WeatherServices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotNetEnv;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using Discord;
+
+namespace WheaterBot
+{
+    public class WeatherServices
+    {
+        public static async Task<string> ObterClimaAsync(string city)
+        {
+            Env.Load("C:\\Users\\Pedro Rossi\\source\\repos\\WheaterBot\\.env");
+
+            string apiKey = Env.GetString("WEATHER_API_KEY");
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                return "Chave da API não encontrada!";
+            }
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric&lang=pt_br";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return "Cidade não encontrada ou erro na API.";
+                }
+
+                string content = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(content);
+
+                string description = json["weather"][0]["description"]?.ToString();
+                double temperature = Math.Round(double.Parse(json["main"]["temp"].ToString()));
+
+                return $"🌦️ **Previsão para `{city.ToUpper()}`**\n" +
+                        "```ini\n" +
+                        $"[Condição]:   {description}\n" +
+                        $"[Temperatura]: {temperature}°C\n" +
+                        "```";
+            }
+
+
+        }
+    }
+}
